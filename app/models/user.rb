5220584c952ac8e2ac08def334b39e708bce2d15
pbsmith82 <pbsmith82@gmail.com>
2021-02-13 18:58:18 -0500

    Added Controllers, views, & routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :walks
     has_many :dogs, through: :walks
 
+    has_secure_password
+    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+ resources :dogs do 
+  resources :walks, only: [:index, :new]
+ end
+ resources :users
+ resources :walks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
