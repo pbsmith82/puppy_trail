2f52a781dca46aeb412e3e90da2890558034cd21
pbsmith82 <pbsmith82@gmail.com>
2021-02-13 20:24:04 -0500

    Added Nav Bar and Edit Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
  end
  resources :users
  resources :walks
+
+ root 'greetings#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
