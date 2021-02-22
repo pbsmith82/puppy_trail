3047cdc51651542513662a0b7a077c849884c691
pbsmith82 <pbsmith82@gmail.com>
2021-02-22 08:06:13 -0500

    Added helper method to clean up walk view ...

--- app/helpers/walks_helper.rb
+++ app/helpers/walks_helper.rb
@@ -18,5 +18,20 @@ module WalksHelper
         end
     end
 
+    
+    def walk_footer
+        out = ''
+        if @dog 
+            out << '<a class="btn btn-secondary btn-sm" href="' + dog_walks_path(@dog) +'" role="button">Cancel</a>'
+        elsif
+            @walk.id != nil
+            out << '<a class="btn btn-danger btn-sm" href="delete" role="button">Delete</a>'
+        else
+            out << '<a class="btn btn-secondary btn-sm" href="' + dogs_path + '" role="button">Cancel</a>'
+        end
+        out.html_safe
+    end
+        
+
 
 end
