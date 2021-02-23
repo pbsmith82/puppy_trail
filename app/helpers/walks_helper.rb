2df6ce30b946278931e5b070dae3c2eac4874c95
pbsmith82 <pbsmith82@gmail.com>
2021-02-23 10:46:08 -0500

    Fixed Cancel Button for Edits

--- app/helpers/walks_helper.rb
+++ app/helpers/walks_helper.rb
@@ -25,6 +25,7 @@ module WalksHelper
             out << '<a class="btn btn-secondary btn-sm" href="' + dog_walks_path(@dog) +'" role="button">Cancel</a>'
         elsif
             @walk.id != nil
+            out << '<a class="btn btn-secondary btn-sm" href="' + dogs_path + '" role="button">Cancel</a> '
             out << '<a class="btn btn-danger btn-sm" href="delete" role="button">Delete</a>'
         else
             out << '<a class="btn btn-secondary btn-sm" href="' + dogs_path + '" role="button">Cancel</a>'
